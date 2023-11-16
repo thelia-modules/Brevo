a2d13ab3c1adfe4ace393a8eeb13cbe563a74d8a
Franck Allimant <fallimant@openstudio.fr>
2023-11-16 12:16:36 +0100

    Fixed tracking code inclusion

--- Hook/HookManager.php
+++ Hook/HookManager.php
@@ -1,14 +1,21 @@
 <?php
-/*************************************************************************************/
-/*      This file is part of the Thelia package.                                     */
-/*                                                                                   */
-/*      Copyright (c) OpenStudio                                                     */
-/*      email : dev@thelia.net                                                       */
-/*      web : http://www.thelia.net                                                  */
-/*                                                                                   */
-/*      For the full copyright and license information, please view the LICENSE.txt  */
-/*      file that was distributed with this source code.                             */
-/*************************************************************************************/
+
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*      Copyright (c) OpenStudio */
+/*      email : dev@thelia.net */
+/*      web : http://www.thelia.net */
+
+/*      For the full copyright and license information, please view the LICENSE.txt */
+/*      file that was distributed with this source code. */
 
 namespace Brevo\Hook;
 
@@ -18,14 +25,12 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Thelia\Core\Event\Hook\HookRenderEvent;
 use Thelia\Core\Hook\BaseHook;
 use Thelia\Core\HttpFoundation\Request;
-use Thelia\Core\Template\Assets\AssetResolverInterface;
 use Thelia\Model\ConfigQuery;
 use Thelia\Model\Customer;
-use TheliaSmarty\Template\SmartyParser;
 
 class HookManager extends BaseHook
 {
-    /** @var Request  */
+    /** @var Request */
     protected $request;
 
     public function __construct(RequestStack $requestStack, EventDispatcherInterface $eventDispatcher)
@@ -35,30 +40,22 @@ class HookManager extends BaseHook
         $this->request = $requestStack->getCurrentRequest();
     }
 
-    public function onModuleConfiguration(HookRenderEvent $event)
+    public function onModuleConfiguration(HookRenderEvent $event): void
     {
         $event->add(
-            $this->render("brevo-configuration.html")
+            $this->render('brevo-configuration.html')
         );
     }
 
-    public function onMainHeadTop(HookRenderEvent $event)
+    public function onMainHeadTop(HookRenderEvent $event): void
     {
-        $apiKey = ConfigQuery::read(Brevo::CONFIG_API_SECRET);
-
-        //$this->get('thelia.customer_user');
         /** @var Customer $customer */
-        $customer = $this->request->getSession()->getCustomerUser();
-
-        $email = null;
-        if ($customer !== null) {
-            $email = $customer->getEmail();
-        }
+        $customer = $this->request->getSession()?->getCustomerUser();
 
         $event->add(
-            $this->render("tracking_script.html",[
-                'apiKey' => $apiKey,
-                'email' => $email
+            $this->render('tracking_script.html', [
+                'marketingAutomationKey' => ConfigQuery::read(Brevo::CONFIG_AUTOMATION_KEY),
+                'email' => $customer?->getEmail(),
             ])
         );
     }
