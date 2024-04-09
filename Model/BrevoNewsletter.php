2174c239310680eefb7e759a433dd586e87f1139
Franck Allimant <fallimant@openstudio.fr>
2024-04-09 14:56:54 +0200

    adding missing model files

---
+++ Model/BrevoNewsletter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Brevo\Model;
+
+use Brevo\Model\Base\BrevoNewsletter as BaseBrevoNewsletter;
+
+/**
+ * Skeleton subclass for representing a row from the 'brevo_newsletter' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class BrevoNewsletter extends BaseBrevoNewsletter
+{
+
+}
---
+++ Model/BrevoNewsletterQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Brevo\Model;
+
+use Brevo\Model\Base\BrevoNewsletterQuery as BaseBrevoNewsletterQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'brevo_newsletter' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class BrevoNewsletterQuery extends BaseBrevoNewsletterQuery
+{
+
+}
