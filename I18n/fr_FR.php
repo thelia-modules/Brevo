a15eac333325807b7bcb55c2368101a8d6b53ea1
lo4k <ryyyansaiyajin@gmail.com>
2024-07-01 17:44:32 +0200

    fix translation

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,15 +1,17 @@
 <?php
 
 return array(
-    'An error occurred during the newsletter registration process' => 'Une erreur s\'est produit pendant l\'enregistrement de votre adresse email',
+    'An error occurred during the newsletter registration process' => 'Une erreur s\'est produite pendant l\'enregistrement de votre adresse email',
     'Automation key' => 'Code de suivi',
-    'Brevo API key' => 'Clef d\'API Brevo',
-    'Click Contacts in the left menu -> Lists and get the list ID without # (e.g. enter 22 if the list ID is #22)' => 'Cliquez Contacts dans le menu de gauche de Brevo, puis Listes et reportez l\'ID de la liste de contacts à utiliser sans le # (ex. indiquez 22 si l\'ID de la liste est #22)',
-    'Contact list ID' => 'Idetifiant de la liste de contacts à utiliser',
+    'Brevo API key' => 'Clé d\'API Brevo',
+    'Click Contacts in the left menu -> Lists and get the list ID without # (e.g. enter 22 if the list ID is #22)' => 'Cliquez sur Contacts dans le menu de gauche de Brevo, puis Listes et reportez l\'ID de la liste de contacts à utiliser sans le # (ex. indiquez 22 si l\'ID de la liste est #22)',
+    'Contact list ID' => 'Identifiant de la liste de contacts à utiliser',
     'Customer attributes mapping' => 'Transcodage des attributs client',
     'The module will throw an error if something wrong happens whan talking to Brevo. Warning ! This could prevent user registration if Brevo server is down or unreachable !' => 'Si cette case est cochée, le module générera une erreur si un problème survient lors du dialogue avec Brevo. Attention ! Cela pourrait empêcher l\'enregistrement des utilisateurs si le serveur Brevo est en panne ou inaccessible !',
     'This is a mapping of Brevo contact attributes with Thelia customer attributes. Do not change anything here if you do not know exactly what you are doing' => 'Le transcodage (exprimé en JSON) des attributs Thelia vers les attributs Brevo. Ne modifiez rien ici si vous ne savez pas exactement ce que vous faites.',
     'Throw exception on Brevo error' => 'Lever une exception en cas d\'erreur retournée par Brevo',
-    'To get a key, select Automation in the left menu -> Settings -> Tracking code -> JS tracer -> copy the client_key value in the JS code displayed (something like k43xd26m9fzeyup9r3nogjxp)' => 'Pour obtenir ce code de suivi, clquez Automatisation dans le menu Brevo, puis Paramètres -> Code de suivi -> Code de suivi JS -> copiez la valeur de client_key dans le code JS affiché (cette valeur est quelque chose comme k43xd26m9fzeyup9r3nogjxp)',
-    'To get an API key, click the top right menu -> SMTP and API -> API Keys -> create a new API Key' => 'Pour obtenir une clef d\'API, cliquez le menu en haut à droite de votre compte Brevo puis SMTP et API -> Clefs d\'API -> créez une nouvelle clef d\'API et copiez la ici.',
+    'To get a key, select Automation in the left menu -> Settings -> Tracking code -> JS tracer -> copy the client_key value in the JS code displayed (something like k43xd26m9fzeyup9r3nogjxp)' => 'Pour obtenir ce code de suivi, cliquez sur Automatisation dans le menu Brevo, puis Paramètres -> Code de suivi -> Code de suivi JS -> copiez la valeur de client_key dans le code JS affiché (cette valeur est quelque chose comme k43xd26m9fzeyup9r3nogjxp)',
+    'To get an API key, click the top right menu -> SMTP and API -> API Keys -> create a new API Key' => 'Pour obtenir une clé d\'API, cliquez sur le menu en haut à droite de votre compte Brevo puis SMTP et API -> Clés d\'API -> créez une nouvelle clé d\'API et copiez-la ici.',
+    'This is a mapping of Brevo products data and meta-data attributes with Thelia products attributes. Do not change anything here if you do not know exactly what you are doing' => 'Il s\'agit d\'une correspondance entre les données et attributs de métadonnées des produits Brevo et les attributs des produits Thelia. Ne modifiez rien ici si vous ne savez pas exactement ce que vous faites.',
+    'Products attributes mapping' => 'Mappage des attributs de produits'
 );
