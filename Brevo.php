5497fc55229e1852fbce62da5c3ed7b4f9e1a924
Bruno Payet <bpayet@openstudio.fr>
2026-03-11 09:50:17 +0100

    feat: adapt_to_thelia3

--- Brevo.php
+++ Brevo.php
@@ -15,7 +15,7 @@ namespace Brevo;
 use Propel\Runtime\Connection\ConnectionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Symfony\Component\Filesystem\Filesystem;
-use Thelia\Install\Database;
+use Thelia\Core\Install\Database;
 use Thelia\Model\Config;
 use Thelia\Model\ConfigQuery;
 use Thelia\Module\BaseModule;
